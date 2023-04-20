79d420b37de38bd11fd37625a554fdb277fd6186
Keith Pickford <Keith@FunctionZero.com>
2023-04-20 14:46:49 +0100

    Add project files.

---
+++ TabbedPageNavBug/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace TabbedPageNavBug;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        var tabbedPage = new TabbedPage();
+        tabbedPage.Children.Add(new ContentPage() { Title = "No Nav root" });
+        tabbedPage.Children.Add(new NavigationPage(new NavContentPage()){ Title = "Has Nav root" });
+        tabbedPage.Children.Add(new NavigationPage(new NavContentPage()){ Title = "Has Nav root" });
+        MainPage = tabbedPage;
+    }
+
+    public class NavContentPage : ContentPage
+    {
+        public NavContentPage()
+        {
+            var button = new Button() { Text = "Push a page", VerticalOptions = LayoutOptions.Start };
+            button.Clicked += (s, e) => { this.Navigation.PushAsync(new NavContentPage()); };
+            Content = button;
+        }
+    }
+}
---
+++ TabbedPageNavBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TabbedPageNavBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
